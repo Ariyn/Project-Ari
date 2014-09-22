545354416fd80554582ca8876fe365f7f96d71f9
rockpell <amthffk@naver.com>
2014-09-22 23:31:51 +0900

    비행기 공항 클래스 구조 만드는 중

--- Engine/Airport.java
+++ Engine/Airport.java
@@ -1,5 +1,41 @@
 package Engine;
 
 public class Airport {
-
+	
+	String name; // 공항명
+	int latitude, altitude; // 공항의 위도, 경도
+	int runwayLength;// 활주로 길이
+	
+	// 비행기 이착륙은 허가 후 이착륙 가능 허가 메소드에서는 큐에 비행기 객체들을 집어넣어 순차적으로 허가시킨다.
+	// 비행기 객체는 트리구조로 저장
+	
+	public Airport(){
+		
+	}
+	
+	public void Position(){ // 공항 위치 보여주는 메소드
+		
+	}
+	
+	public void LandingPermitSign(){ // 비행기 착륙 허가
+		
+	}
+	
+	public void PlaneLanding(){ // 비행기 착륙
+		
+	}
+	
+	public void TakeOffPermitSign(){ // 비행기 이륙 허가
+		
+	}
+	
+	public void PlaneTakeOff(){ // 비행기 이륙
+		
+	}
+	
+	public void OnPlane(){ // 현재 공항에 체류중인 비행기
+		
+	}
+	
+	
 }
--- Engine/Plane.java
+++ Engine/Plane.java
@@ -1,11 +1,40 @@
 package Engine;
 
 public class Plane {
-	public int code, weight;
-	int fuel, fuelTank, fuelPercent;
+	public int code, weight, length; // 비행기 기종, 무게, 길이
+	int fuel, fuelTank, fuelPercent; // 연료, 연료최대량, 연료량(%)
+	int speed, height; // 속도, 고도
+	int x,y; // 좌표 
+	int latitude, altitude; // 위도, 경도
+	
+	// 위도와 경도를 3600으로 나눈후 중간을 좌표 0으로 지정 좌우로 +-500씩 할당한다
+	// 좌표가 +-500를 초과하면 위도 또는 경도를 변경한다.
+	
+	boolean hiJack; // 하이잭 유무
 	
 	public Plane(){
 		code = 10;
 	}
 	
+	public void Position(){ // 비행기 위치 보여주는 메소드
+		
+	}
+	
+	public void Move(){ // 비행기 좌표 이동(연료 소모 표시)
+		
+	}
+
+	/*public void Search(){ // 주변 비행기 탐색
+		
+	}*/
+	
+	public void Data(){ // 비행기 정보 출력
+		
+	}
+	
+	public boolean LandingSign(){ // 공항에 착륙 허가 신청
+		boolean sign = true;
+		
+		return sign;
+	}
 }
